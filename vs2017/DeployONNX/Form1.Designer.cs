e4ae40ed43bba5499340696c42b2e7069d776d21
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2024-02-26 05:40:58 +0900

    Add files via upload

---
+++ vs2017/DeployONNX/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace DeployONNX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage_ONNX = new System.Windows.Forms.TabPage();
+            this.tabPage_CreateTestImage = new System.Windows.Forms.TabPage();
+            this.textBox_CreateTestImage_TargetDir = new System.Windows.Forms.TextBox();
+            this.button_CreateTestImage_Run = new System.Windows.Forms.Button();
+            this.textBox_CreateTestImage_Size = new System.Windows.Forms.TextBox();
+            this.textBox_LoadOnnxFilename = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_LoadOnnxFile = new System.Windows.Forms.Button();
+            this.textBox_LoadOnnxFileInfo_Size = new System.Windows.Forms.TextBox();
+            this.textBox_LoadOnnxFileInfo = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage_ONNX.SuspendLayout();
+            this.tabPage_CreateTestImage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage_ONNX);
+            this.tabControl1.Controls.Add(this.tabPage_CreateTestImage);
+            this.tabControl1.Location = new System.Drawing.Point(6, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(552, 388);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage_ONNX
+            // 
+            this.tabPage_ONNX.Controls.Add(this.textBox_LoadOnnxFileInfo);
+            this.tabPage_ONNX.Controls.Add(this.textBox_LoadOnnxFileInfo_Size);
+            this.tabPage_ONNX.Controls.Add(this.button_LoadOnnxFile);
+            this.tabPage_ONNX.Controls.Add(this.label1);
+            this.tabPage_ONNX.Controls.Add(this.textBox_LoadOnnxFilename);
+            this.tabPage_ONNX.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_ONNX.Name = "tabPage_ONNX";
+            this.tabPage_ONNX.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_ONNX.Size = new System.Drawing.Size(544, 362);
+            this.tabPage_ONNX.TabIndex = 0;
+            this.tabPage_ONNX.Text = "ONNX";
+            this.tabPage_ONNX.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_CreateTestImage
+            // 
+            this.tabPage_CreateTestImage.Controls.Add(this.textBox_CreateTestImage_Size);
+            this.tabPage_CreateTestImage.Controls.Add(this.button_CreateTestImage_Run);
+            this.tabPage_CreateTestImage.Controls.Add(this.textBox_CreateTestImage_TargetDir);
+            this.tabPage_CreateTestImage.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_CreateTestImage.Name = "tabPage_CreateTestImage";
+            this.tabPage_CreateTestImage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_CreateTestImage.Size = new System.Drawing.Size(357, 202);
+            this.tabPage_CreateTestImage.TabIndex = 1;
+            this.tabPage_CreateTestImage.Text = "CreateTestImage";
+            this.tabPage_CreateTestImage.UseVisualStyleBackColor = true;
+            // 
+            // textBox_CreateTestImage_TargetDir
+            // 
+            this.textBox_CreateTestImage_TargetDir.Location = new System.Drawing.Point(30, 49);
+            this.textBox_CreateTestImage_TargetDir.Name = "textBox_CreateTestImage_TargetDir";
+            this.textBox_CreateTestImage_TargetDir.Size = new System.Drawing.Size(262, 19);
+            this.textBox_CreateTestImage_TargetDir.TabIndex = 0;
+            // 
+            // button_CreateTestImage_Run
+            // 
+            this.button_CreateTestImage_Run.Location = new System.Drawing.Point(30, 20);
+            this.button_CreateTestImage_Run.Name = "button_CreateTestImage_Run";
+            this.button_CreateTestImage_Run.Size = new System.Drawing.Size(75, 23);
+            this.button_CreateTestImage_Run.TabIndex = 1;
+            this.button_CreateTestImage_Run.Text = "Run";
+            this.button_CreateTestImage_Run.UseVisualStyleBackColor = true;
+            this.button_CreateTestImage_Run.Click += new System.EventHandler(this.button_CreateTestImage_Run_Click);
+            // 
+            // textBox_CreateTestImage_Size
+            // 
+            this.textBox_CreateTestImage_Size.Location = new System.Drawing.Point(40, 96);
+            this.textBox_CreateTestImage_Size.Name = "textBox_CreateTestImage_Size";
+            this.textBox_CreateTestImage_Size.Size = new System.Drawing.Size(100, 19);
+            this.textBox_CreateTestImage_Size.TabIndex = 2;
+            this.textBox_CreateTestImage_Size.Text = "224,224";
+            // 
+            // textBox_LoadOnnxFilename
+            // 
+            this.textBox_LoadOnnxFilename.Location = new System.Drawing.Point(6, 26);
+            this.textBox_LoadOnnxFilename.Name = "textBox_LoadOnnxFilename";
+            this.textBox_LoadOnnxFilename.Size = new System.Drawing.Size(521, 19);
+            this.textBox_LoadOnnxFilename.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LoadOnnxFilename";
+            // 
+            // button_LoadOnnxFile
+            // 
+            this.button_LoadOnnxFile.Location = new System.Drawing.Point(8, 61);
+            this.button_LoadOnnxFile.Name = "button_LoadOnnxFile";
+            this.button_LoadOnnxFile.Size = new System.Drawing.Size(75, 23);
+            this.button_LoadOnnxFile.TabIndex = 2;
+            this.button_LoadOnnxFile.Text = "Load";
+            this.button_LoadOnnxFile.UseVisualStyleBackColor = true;
+            this.button_LoadOnnxFile.Click += new System.EventHandler(this.button_LoadOnnxFile_Click);
+            // 
+            // textBox_LoadOnnxFileInfo_Size
+            // 
+            this.textBox_LoadOnnxFileInfo_Size.Location = new System.Drawing.Point(126, 64);
+            this.textBox_LoadOnnxFileInfo_Size.Name = "textBox_LoadOnnxFileInfo_Size";
+            this.textBox_LoadOnnxFileInfo_Size.Size = new System.Drawing.Size(100, 19);
+            this.textBox_LoadOnnxFileInfo_Size.TabIndex = 3;
+            // 
+            // textBox_LoadOnnxFileInfo
+            // 
+            this.textBox_LoadOnnxFileInfo.Location = new System.Drawing.Point(13, 93);
+            this.textBox_LoadOnnxFileInfo.Multiline = true;
+            this.textBox_LoadOnnxFileInfo.Name = "textBox_LoadOnnxFileInfo";
+            this.textBox_LoadOnnxFileInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_LoadOnnxFileInfo.Size = new System.Drawing.Size(513, 251);
+            this.textBox_LoadOnnxFileInfo.TabIndex = 4;
+            this.textBox_LoadOnnxFileInfo.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 405);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "DeployONNX";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage_ONNX.ResumeLayout(false);
+            this.tabPage_ONNX.PerformLayout();
+            this.tabPage_CreateTestImage.ResumeLayout(false);
+            this.tabPage_CreateTestImage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage_ONNX;
+        private System.Windows.Forms.TabPage tabPage_CreateTestImage;
+        private System.Windows.Forms.Button button_CreateTestImage_Run;
+        private System.Windows.Forms.TextBox textBox_CreateTestImage_TargetDir;
+        private System.Windows.Forms.TextBox textBox_CreateTestImage_Size;
+        private System.Windows.Forms.Button button_LoadOnnxFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_LoadOnnxFilename;
+        private System.Windows.Forms.TextBox textBox_LoadOnnxFileInfo;
+        private System.Windows.Forms.TextBox textBox_LoadOnnxFileInfo_Size;
+    }
+}
+
---
+++ vs2017/DeployONNX/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using WinFormStringCnvClass;
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+
+namespace DeployONNX
+{
+    public partial class Form1 : Form
+    {
+        string thisExeDirPath;
+        public Form1()
+        {
+            InitializeComponent();
+            thisExeDirPath = Path.GetDirectoryName(Application.ExecutablePath);
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "TEXT|*.txt";
+            if (false && ofd.ShowDialog() == DialogResult.OK)
+            {
+                WinFormStringCnv.setControlFromString(this, File.ReadAllText(ofd.FileName));
+            }
+            else
+            {
+                string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+                if (File.Exists(paramFilename))
+                {
+                    WinFormStringCnv.setControlFromString(this, File.ReadAllText(paramFilename));
+                }
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string FormContents = WinFormStringCnv.ToString(this);
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "TEXT|*.txt";
+
+            if (false && sfd.ShowDialog() == DialogResult.OK)
+            {
+
+                File.WriteAllText(sfd.FileName, FormContents);
+            }
+            else
+            {
+                string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+                File.WriteAllText(paramFilename, FormContents);
+            }
+
+        }
+
+        private void button_CreateTestImage_Run_Click(object sender, EventArgs e)
+        {
+            string topDirectoryPath = textBox_CreateTestImage_TargetDir.Text;
+            if (!Directory.Exists(topDirectoryPath)) Directory.CreateDirectory(topDirectoryPath);
+            if (!Directory.Exists(topDirectoryPath)) return;
+
+            string imageSizeString = textBox_CreateTestImage_Size.Text;
+            float defSize = 20.0f;
+
+            //RedDot
+            createTestImage_Dot(Path.Combine(topDirectoryPath, "01", "RedDot.png"), imageSizeString, defSize, 50, Brushes.Red);
+
+            //BlueDot
+            createTestImage_Dot(Path.Combine(topDirectoryPath, "02", "BlueDot.png"), imageSizeString, defSize, 50, Brushes.Blue);
+
+            //BlackDot
+            createTestImage_Dot(Path.Combine(topDirectoryPath, "03", "BlackDot.png"), imageSizeString, defSize, 50, Brushes.Black);
+
+            defSize = 50.0f;
+            //RedFlag
+            createTestImage_Flagment(Path.Combine(topDirectoryPath, "04", "RedFlag.png"), imageSizeString, defSize, 50, Brushes.Red);
+
+            //BlueFlag
+            createTestImage_Flagment(Path.Combine(topDirectoryPath, "05", "BlueFlag.png"), imageSizeString, defSize, 50, Brushes.Blue);
+
+            //BlackFlag
+            createTestImage_Flagment(Path.Combine(topDirectoryPath, "06", "BlackFlag.png"), imageSizeString, defSize, 50, Brushes.Black);
+
+
+        }
+
+
+
+
+        private void createTestImage_Dot(string filename, string imageSize, float dotDiameter, int imgCount, Brush brush)
+        {
+            string topDirectoryPath = Path.GetDirectoryName(filename);
+            if (!Directory.Exists(topDirectoryPath)) Directory.CreateDirectory(topDirectoryPath);
+
+            string[] sizeArray = imageSize.Trim(',').Split(',');
+
+            int imgWidth = int.Parse(sizeArray[0]);
+            int imgHeight = int.Parse(sizeArray[1]);
+
+            Random rnd = new Random();
+
+            for (int imgIndex = 0; imgIndex < imgCount; imgIndex++)
+            {
+                using (Bitmap img = new Bitmap(imgWidth, imgHeight, PixelFormat.Format24bppRgb))
+                {
+
+                    float cx = (float)(imgWidth / 2.0 + (rnd.NextDouble() - 0.5) * imgWidth / 2.0);
+                    float cy = (float)(imgHeight / 2.0 + (rnd.NextDouble() - 0.5) * imgHeight / 2.0);
+                    float d = (float)(dotDiameter + dotDiameter * 0.5 * (rnd.NextDouble() * 2.0 - 1.0));
+
+                    using (Graphics g = Graphics.FromImage(img))
+                    {
+                        g.FillRectangle(Brushes.White, 0, 0, imgWidth, imgHeight);
+                        g.FillEllipse(brush, cx - d / 2.0f, cy - d / 2.0f, d / 2.0f, d / 2.0f);
+
+                    }
+
+                    string saveFilename = Path.Combine(Path.GetDirectoryName(filename), Path.GetFileNameWithoutExtension(filename) + imgIndex.ToString("0000") + Path.GetExtension(filename));
+                    img.Save(saveFilename);
+
+                }
+
+            }
+
+        }
+
+        private void createTestImage_Flagment(string filename, string imageSize, float dotDiameter, int imgCount, Brush brush)
+        {
+            string topDirectoryPath = Path.GetDirectoryName(filename);
+            if (!Directory.Exists(topDirectoryPath)) Directory.CreateDirectory(topDirectoryPath);
+
+            string[] sizeArray = imageSize.Trim(',').Split(',');
+
+            int imgWidth = int.Parse(sizeArray[0]);
+            int imgHeight = int.Parse(sizeArray[1]);
+
+            Random rnd = new Random();
+
+            for (int imgIndex = 0; imgIndex < imgCount; imgIndex++)
+            {
+                using (Bitmap img = new Bitmap(imgWidth, imgHeight, PixelFormat.Format24bppRgb))
+                {
+
+                    float cx = (float)(imgWidth / 2.0 + (rnd.NextDouble() - 0.5) * imgWidth / 2.0);
+                    float cy = (float)(imgHeight / 2.0 + (rnd.NextDouble() - 0.5) * imgHeight / 2.0);
+
+                    List<Point> points = new List<Point>();
+
+                    for (int corIndex = 0; corIndex < 3; corIndex++)
+                    {
+                        float dx = (float)(dotDiameter + dotDiameter * 0.5 * (rnd.NextDouble() * 2.0 - 1.0));
+                        float dy = (float)(dotDiameter + dotDiameter * 0.5 * (rnd.NextDouble() * 2.0 - 1.0));
+                        points.Add(new Point((int)(cx - dx / 2.0f), (int)(cy - dy / 2.0f)));
+                    }
+
+                    double areaCount = CalculateTriangleArea(points);
+                    if (areaCount <= dotDiameter * 1.5f)
+                    {
+                        imgIndex--;
+                    }
+                    else
+                    {
+                        using (Graphics g = Graphics.FromImage(img))
+                        {
+                            g.FillRectangle(Brushes.White, 0, 0, imgWidth, imgHeight);
+                            g.FillPolygon(brush, points.ToArray());
+
+                        }
+
+                        string saveFilename = Path.Combine(Path.GetDirectoryName(filename), Path.GetFileNameWithoutExtension(filename) + imgIndex.ToString("0000") + Path.GetExtension(filename));
+                        img.Save(saveFilename);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        double CalculateTriangleArea(List<Point> points)
+        {
+            if (points.Count != 3)
+            {
+                throw new ArgumentException("Exactly 3 points are required to calculate the area of a triangle.");
+            }
+
+            double area = Math.Abs((points[0].X * (points[1].Y - points[2].Y) +
+                                    points[1].X * (points[2].Y - points[0].Y) +
+                                    points[2].X * (points[0].Y - points[1].Y)) / 2.0);
+            return area;
+        }
+
+        private void button_LoadOnnxFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "ONNX|*.onnx";
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            string modelPath = ofd.FileName;
+            textBox_LoadOnnxFilename.Text = modelPath;
+
+            List<string> Lines = new List<string>();
+
+            using (var session = new InferenceSession(modelPath))
+            {
+                Lines.Add($"ProducerName: {session.ModelMetadata.ProducerName}");
+                Lines.Add($"Version: {session.ModelMetadata.Version.ToString()}");
+                Lines.Add($"Description: {session.ModelMetadata.Description}");
+
+                Lines.Add("//Input//");
+                foreach (var input in session.InputMetadata)
+                {
+                    Lines.Add($" Key: {input.Key}, \r\n Value.ElementType: {input.Value.ElementType},\r\n Value.Dimensions: {string.Join(",", input.Value.Dimensions)}");
+                    int[] Dimensions = input.Value.Dimensions.ToArray();
+
+                    textBox_LoadOnnxFileInfo_Size.Text = Dimensions[2].ToString() + "," + Dimensions[3].ToString();
+                }
+
+                Lines.Add("//Output//");
+                foreach (var output in session.OutputMetadata)
+                {
+                    Lines.Add($" Key: {output.Key},\r\n Value.ElementType: {output.Value.ElementType},\r\n Value.Dimensions: {string.Join(",", output.Value.Dimensions)}");
+                }
+
+            };
+
+            textBox_LoadOnnxFileInfo.Text = string.Join("\r\n", Lines.ToArray());
+
+        }
+    }
+}
